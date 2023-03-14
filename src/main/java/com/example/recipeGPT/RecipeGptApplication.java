7b2b3d2d3b04d686405f954d5d668786539ae5aa
youngmin <min050410@gmail.com>
2023-03-14 15:35:02 +0900

    init: spring / java / jpa 프로젝트 생성, yml 설정

---
+++ src/main/java/com/example/recipeGPT/RecipeGptApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipeGPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeGptApplication.class, args);
+	}
+
+}
