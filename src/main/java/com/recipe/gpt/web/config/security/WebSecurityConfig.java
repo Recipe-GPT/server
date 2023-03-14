1f5a1285b567d39e1af922174f7888dc020b80a5
youngmin <min050410@gmail.com>
2023-03-14 16:31:25 +0900

    add: api path 상수 관리

--- src/main/java/com/recipe/gpt/web/config/security/WebSecurityConfig.java
+++ src/main/java/com/recipe/gpt/web/config/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.recipe.gpt.web.config.security;
 
+import com.recipe.gpt.web.path.ApiPath;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/com/recipe/gpt/web/path/ApiPath.java
@@ -0,0 +1,8 @@
+package com.recipe.gpt.web.path;
+
+public class ApiPath {
+
+    // 에러 핸들러
+    public static final String ERROR_AUTH = "/api/error";
+
+}
