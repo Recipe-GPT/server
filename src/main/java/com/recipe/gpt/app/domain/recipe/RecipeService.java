3a7826de1c65a3bec98d91ee554fe832c602d66b
min050410 <min050410@gmail.com>
2023-05-05 23:53:06 +0900

    test: 레시피 prompt 테스트

--- src/main/java/com/recipe/gpt/app/domain/recipe/RecipeService.java
+++ src/main/java/com/recipe/gpt/app/domain/recipe/RecipeService.java
@@ -15,10 +15,46 @@ public class RecipeService {
 
     public ResponseEntity<?> recipeQuery(OpenAiRequestDto body) {
 
+        final String prompt = """
+            An recipe recommendation assistant that recommends three delicious food recipes with given ingredients. The recommendation assistant responds only in JSON key-value format. It will not provide any other unnecessary responses.
+            USER:
+            [ingredients]
+            Cheddar cheese, Bread, Pork Belly, Rice, Cabbage, Scallions, Potatoes, Eggs, Bracken, Onions
+            [condiments]
+            Pepper, Salt, Soy sauce, Gochujang, Sugar, Mustard, Ketchup
+            Assistant:
+            [{
+              "name": "Kimchi Pork Belly Sandwich",
+              "description": "A delicious sandwich with kimchi and pork belly",
+              "ingredients": ["bread", "pork belly", "kimchi", "cheddar cheese"],
+              "condiments": ["mustard", "ketchup"],
+              "cooking_time_second": 900
+            },
+            {
+              "name": "Cheese Pork Belly Rice Balls",
+              "description": "A delicious rice ball made with pork belly and cheddar cheese",
+              "ingredients": ["rice", "pork belly", "cheddar cheese", "green onion"],
+              "condiments": ["gochujang", "soy sauce", "sugar"],
+              "cooking_time_second": 1200
+            },
+            {
+              "name": "Egg roll",
+              "description": "A food made by spreading an egg wide and rolling it up",
+              "ingredients": ["egg", "scallion"],
+              "condiments": ["salt", "pepper"],
+              "cooking_time_second": 600
+            }]</s>
+            USER:
+            [ingredients]
+            Mozzarella cheese, Pork belly, Bread, Rice, Cabbage, Scallions, Potatoes, Fish, Eggs
+            [condiments]
+            Pepper, Gochujang, Salt, Soy Sauce, Bulgogi Sauce, Gochujang, Sugar, Mayonnaise, Ketchup
+            Assistant:""";
+
         OpenAiService service = new OpenAiService(
             key);
         CompletionRequest completionRequest = CompletionRequest.builder()
-            .prompt(body.getPrompt())
+            .prompt(prompt)
             .model("text-davinci-003")
             .echo(false)
             .bestOf(1)
