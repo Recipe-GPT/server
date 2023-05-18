e9e0e914b0dc5555a0dbb5d6e3483cca05317234
min050410 <min050410@gmail.com>
2023-05-18 20:13:42 +0900

    [#1] feat(Api): AiServerResponseDto 작성

---
+++ src/main/java/com/recipe/gpt/app/web/dto/recipe/ai/AiServerResponseDto.java
@@ -0,0 +1,22 @@
+package com.recipe.gpt.app.web.dto.recipe.ai;
+
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class AiServerResponseDto {
+
+    private String name;
+
+    private String description;
+
+    private List<String> ingredients;
+
+    private List<String> seasonings;
+
+}
