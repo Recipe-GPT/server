83a60f7a8598528df208df4e30bfcc6a8432574d
min050410 <min050410@gmail.com>
2023-05-18 20:14:07 +0900

    [#1] feat(Api): httpExceptionHandler 작성

---
+++ src/main/java/com/recipe/gpt/app/web/response/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.recipe.gpt.app.web.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ErrorResponse {
+
+    private Integer statusCode;
+    private String reason;
+    private String message;
+
+}
