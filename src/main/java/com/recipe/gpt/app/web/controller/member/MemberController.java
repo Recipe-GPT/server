a1f8eb4173f1f24797d089975385eb3eda383f25
min050410 <min050410@gmail.com>
2023-05-22 15:45:21 +0900

    fix: 태그 이름 멤버로 수정

--- src/main/java/com/recipe/gpt/app/web/controller/member/MemberController.java
+++ src/main/java/com/recipe/gpt/app/web/controller/member/MemberController.java
@@ -1,26 +1,13 @@
 package com.recipe.gpt.app.web.controller.member;
 
-import com.recipe.gpt.app.web.dto.user.OAuth2RequestDto;
-import com.recipe.gpt.app.web.dto.common.TokenDto;
-import com.recipe.gpt.app.web.path.ApiPath;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "인증")
-@RestController
+@Tag(name = "멤버")
 @RequiredArgsConstructor
+@RestController
 public class MemberController {
-
-    @Operation(summary = "OAuth 로그인")
-    @PostMapping(ApiPath.LOGIN_OAUTH2)
-    public TokenDto loginOAuth2(@Validated @RequestBody OAuth2RequestDto body) {
-        return null;
-    }
 
 
 }
