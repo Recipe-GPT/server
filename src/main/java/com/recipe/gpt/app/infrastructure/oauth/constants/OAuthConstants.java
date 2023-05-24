fe65f14c27ade1178aa821234fc71176d5506c84
min050410 <min050410@gmail.com>
2023-05-24 20:42:40 +0900

    [#3] feat(OAuth): OAuthConstants 작성

---
+++ src/main/java/com/recipe/gpt/app/infrastructure/oauth/constants/OAuthConstants.java
@@ -0,0 +1,13 @@
+package com.recipe.gpt.app.infrastructure.oauth.constants;
+
+public class OAuthConstants {
+
+    public static final String GRANT_TYPE = "grant_type";
+    public static final String CLIENT_ID = "client_id";
+    public static final String CLIENT_SECRET = "client_secret";
+    public static final String REDIRECT_URI = "redirect_uri";
+    public static final String CODE = "code";
+    public static final String GOOGLE_AUTH_URL = "https://oauth2.googleapis.com/token";
+    public static final String AUTHORIZATION_CODE = "authorization_code";
+
+}
