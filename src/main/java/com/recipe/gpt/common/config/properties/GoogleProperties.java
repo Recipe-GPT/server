668c78682b8a4b6b0f5facf2082b4b77dcf4ceba
min050410 <min050410@gmail.com>
2023-05-24 20:30:35 +0900

    refac: ConfigurationProperties 경로 수정

--- src/main/java/com/recipe/gpt/common/config/properties/GoogleProperties.java
+++ src/main/java/com/recipe/gpt/common/config/properties/GoogleProperties.java
@@ -1,6 +1,5 @@
 package com.recipe.gpt.common.config.properties;
 
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +7,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PUBLIC)
-@ConfigurationProperties("oauth.google")
+@ConfigurationProperties("spring.security.oauth2.client.registration.google")
 public class GoogleProperties {
 
     private final String clientId;
+
     private final String clientSecret;
-    private final String oAuthEndPoint;
-    private final String responseType;
-    private final List<String> scopes;
-    private final String tokenUri;
-    private final String accessType;
+
     private final String redirectUri;
 
 
