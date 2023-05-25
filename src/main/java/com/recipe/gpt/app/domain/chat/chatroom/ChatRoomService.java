dc19cbc98c15401512510e2e5f1af5a564195ea2
min050410 <min050410@gmail.com>
2023-05-25 17:50:52 +0900

    [#8] feat(ChatRoom): 채팅방 삭제

--- src/main/java/com/recipe/gpt/app/domain/chat/chatroom/ChatRoomService.java
+++ src/main/java/com/recipe/gpt/app/domain/chat/chatroom/ChatRoomService.java
@@ -3,7 +3,12 @@ package com.recipe.gpt.app.domain.chat.chatroom;
 import com.recipe.gpt.app.domain.member.Member;
 import com.recipe.gpt.app.domain.member.MemberService;
 import com.recipe.gpt.app.web.dto.chat.chatroom.ChatRoomRequestDto;
+import com.recipe.gpt.app.web.dto.chat.chatroom.ChatRoomResponseDto;
+import com.recipe.gpt.app.web.response.ListResponse;
 import com.recipe.gpt.common.config.security.context.LoginMember;
+import com.recipe.gpt.common.exception.ChatRoomNotFoundException;
+import com.recipe.gpt.common.exception.NotPossibleToAccessChatRoomException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,5 +28,29 @@ public class ChatRoomService {
         ChatRoom chatRoom = chatRoomRepository.save(body.toChatRoom(member));
         return chatRoom.getId();
     }
+
+    @Transactional
+    public void deleteChatRoom(LoginMember loginMember, Long id) {
+        Member member = memberService.findLoginMember(loginMember);
+
+        ChatRoom chatRoom = findById(id);
+
+        if (chatRoom.isNotPossibleToAccessPlaylist(member)) {
+            throw new NotPossibleToAccessChatRoomException();
+        }
+
+        chatRoomRepository.delete(chatRoom);
+    }
+
+    @Transactional(readOnly = true)
+    public ListResponse<ChatRoomResponseDto> findChatRoom(LoginMember loginMember) {
+        Member member = memberService.findLoginMember(loginMember);
+
+        List<ChatRoom> chatRoomList = chatRoomRepository.findChatRoomsByMember(member);
+        List<ChatRoomResponseDto> chatRoomResponseDtoList = ChatRoomResponseDto.listOf(
+            chatRoomList);
+        return ListResponse.create(chatRoomResponseDtoList);
+    }
+
 
 }
