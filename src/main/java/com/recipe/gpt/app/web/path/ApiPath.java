a6cff9fef6cf2b90cc02aa95d56f7fae43d7f008
min050410 <min050410@gmail.com>
2023-06-07 10:28:47 +0900

    [#10] feat(Chat): ApiPath ChatRoomId 추가

--- src/main/java/com/recipe/gpt/app/web/path/ApiPath.java
+++ src/main/java/com/recipe/gpt/app/web/path/ApiPath.java
@@ -15,8 +15,8 @@ public class ApiPath {
     public static final String MEMBER = "/api/v1/member";
 
     // 레시피 질문
-    public static final String QUERY_RECOMMEND = "/api/v1/query/recommend";
-    public static final String QUERY_RECIPE = "/api/v1/query/recipe";
+    public static final String QUERY_RECOMMEND = "/api/v1/query/recommend/{chatRoomId}";
+    public static final String QUERY_RECIPE = "/api/v1/query/recipe/{chatRoomId}";
 
     // 식재료, 양념 검색
     public static final String SEARCH_INGREDIENT = "/api/v1/search/ingredient";
