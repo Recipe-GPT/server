ea465d252cdfc52323f5e3307254d0f6c135d47c
min050410 <min050410@gmail.com>
2023-06-07 10:26:38 +0900

    feat(Entity): Chat Entity

--- src/main/java/com/recipe/gpt/app/domain/chat/Chat.java
+++ src/main/java/com/recipe/gpt/app/domain/chat/Chat.java
@@ -1,17 +1,26 @@
 package com.recipe.gpt.app.domain.chat;
 
 import com.recipe.gpt.app.domain.chat.chatroom.ChatRoom;
+import com.recipe.gpt.app.domain.chat.recommendrecipe.RecommendRecipe;
+import com.recipe.gpt.app.domain.chat.recommendrecipe.RecommendRecipeItem;
+import com.recipe.gpt.app.domain.chat.requested.ingredient.RequestedIngredient;
+import com.recipe.gpt.app.domain.chat.requested.ingredient.RequestedIngredientItem;
+import com.recipe.gpt.app.domain.chat.requested.seasoning.RequestedSeasoning;
+import com.recipe.gpt.app.domain.chat.requested.seasoning.RequestedSeasoningItem;
 import com.recipe.gpt.app.domain.member.Member;
-import jakarta.persistence.Column;
+import com.recipe.gpt.app.domain.recipe.Recipe;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import java.util.List;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -24,10 +33,6 @@ public class Chat {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Lob
-    @Column(nullable = false, length = 100)
-    private String message;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
@@ -36,6 +41,68 @@ public class Chat {
     @JoinColumn(name = "chatroom_id")
     private ChatRoom chatRoom;
 
-    // TODO :: chat service 구현시 필드 추가
+    @Embedded
+    private RequestedIngredient requestedIngredient = RequestedIngredient.empty();
+
+    @Embedded
+    private RequestedSeasoning requestedSeasoning = RequestedSeasoning.empty();
+
+    @Embedded
+    private RecommendRecipe recommendRecipe = RecommendRecipe.empty();
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe_id")
+    private Recipe recipe;
+
+    public boolean isAccessibleToChat(Member member) {
+        if (member == null) {
+            return false;
+        }
+
+        Long loginMemberId = member.getId();
+        Long memberId = this.member.getId();
+        return memberId.equals(loginMemberId);
+    }
+
+    @Builder
+    private Chat(Member member, ChatRoom chatRoom) {
+        this.member = member;
+        this.chatRoom = chatRoom;
+    }
+
+    public Chat update(Chat requestChat,
+        List<RequestedIngredientItem> requestedIngredients,
+        List<RequestedSeasoningItem> requestedSeasonings,
+        List<RecommendRecipeItem> recommendRecipes) {
+        updateRequestedIngredients(requestedIngredients);
+        updateRequestedSeasonings(requestedSeasonings);
+        updateRecommendRecipes(recommendRecipes);
+        return this;
+    }
+
+    private void updateRequestedIngredients(
+        List<RequestedIngredientItem> requestedIngredientItems) {
+        this.requestedIngredient.clear();
+
+        for (RequestedIngredientItem requestedIngredientItem : requestedIngredientItems) {
+            requestedIngredientItem.setChat(this);
+        }
+    }
+
+    private void updateRequestedSeasonings(List<RequestedSeasoningItem> requestedSeasoningItems) {
+        this.requestedSeasoning.clear();
+
+        for (RequestedSeasoningItem requestedSeasoningItem : requestedSeasoningItems) {
+            requestedSeasoningItem.setChat(this);
+        }
+    }
+
+    private void updateRecommendRecipes(List<RecommendRecipeItem> recommendRecipeItems) {
+        this.recommendRecipe.clear();
+
+        for (RecommendRecipeItem recommendRecipeItem : recommendRecipeItems) {
+            recommendRecipeItem.setChat(this);
+        }
+    }
 
 }
