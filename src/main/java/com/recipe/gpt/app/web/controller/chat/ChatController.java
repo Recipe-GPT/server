b59d8bd474e11676b179cc2dd404f007655722ca
min050410 <min050410@gmail.com>
2023-06-07 10:28:24 +0900

    [#10] feat(Chat): 채팅 엔드포인트에 chatRoomId 추가

--- src/main/java/com/recipe/gpt/app/web/controller/chat/ChatController.java
+++ src/main/java/com/recipe/gpt/app/web/controller/chat/ChatController.java
@@ -1,4 +1,4 @@
-package com.recipe.gpt.app.web.controller.recipe;
+package com.recipe.gpt.app.web.controller.chat;
 
 import com.recipe.gpt.app.domain.chat.ChatService;
 import com.recipe.gpt.app.web.dto.recipe.ai.AiServerRecipeRequestDto;
@@ -7,36 +7,43 @@ import com.recipe.gpt.app.web.dto.recipe.ai.AiServerRecommendResponseDto;
 import com.recipe.gpt.app.web.dto.recipe.ai.ExtractedRecipeResponseDto;
 import com.recipe.gpt.app.web.path.ApiPath;
 import com.recipe.gpt.app.web.response.ListResponse;
+import com.recipe.gpt.common.config.security.context.LoginMember;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "레시피")
+@Tag(name = "AI 채팅")
 @RestController
 @RequiredArgsConstructor
-public class RecipeController {
+public class ChatController {
 
     private final ChatService chatService;
 
     @Operation(summary = "요리 추천 질문")
     @PostMapping(ApiPath.QUERY_RECOMMEND)
     public ResponseEntity<ListResponse<AiServerRecommendResponseDto>> recommendQuery(
+        @AuthenticationPrincipal LoginMember loginMember,
+        @PathVariable Long chatRoomId,
         @Valid @RequestBody AiServerRecommendRequestDto body
     ) {
-        return ResponseEntity.ok(chatService.recommendQuery(body));
+        return ResponseEntity.ok(chatService.recommendQuery(loginMember, chatRoomId, body));
     }
 
     @Operation(summary = "레시피 질문")
     @PostMapping(ApiPath.QUERY_RECIPE)
     public ResponseEntity<ExtractedRecipeResponseDto> recipeQuery(
+        @AuthenticationPrincipal LoginMember loginMember,
+        @PathVariable Long chatRoomId,
         @Valid @RequestBody AiServerRecipeRequestDto body
     ) {
-        return ResponseEntity.ok(chatService.recipeQuery(body));
+        return ResponseEntity.ok(chatService.recipeQuery(loginMember, chatRoomId, body));
     }
 
 }
