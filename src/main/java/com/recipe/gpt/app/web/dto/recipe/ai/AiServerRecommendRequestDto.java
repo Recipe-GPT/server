bdce881b49d0d35bb127d30d0df4310e60f6833f
min050410 <min050410@gmail.com>
2023-06-07 10:29:07 +0900

    [#10] feat(Chat): toRequestedIngredientItem, toRequestedSeasoningItem 메서드 추가

--- src/main/java/com/recipe/gpt/app/web/dto/recipe/ai/AiServerRecommendRequestDto.java
+++ src/main/java/com/recipe/gpt/app/web/dto/recipe/ai/AiServerRecommendRequestDto.java
@@ -1,8 +1,11 @@
 package com.recipe.gpt.app.web.dto.recipe.ai;
 
+import com.recipe.gpt.app.domain.chat.requested.ingredient.RequestedIngredientItem;
+import com.recipe.gpt.app.domain.chat.requested.seasoning.RequestedSeasoningItem;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -21,5 +24,27 @@ public class AiServerRecommendRequestDto {
     @NotEmpty
     @Size(min = 3, max = 20)
     private List<@NotBlank @Size(max = 10) String> seasonings;
+
+    public List<RequestedIngredientItem> toRequestedIngredientItems() {
+        List<RequestedIngredientItem> requestedIngredientItems = new ArrayList<>();
+        for (String ingredient : ingredients) {
+            RequestedIngredientItem item = RequestedIngredientItem.builder()
+                .name(ingredient)
+                .build();
+            requestedIngredientItems.add(item);
+        }
+        return requestedIngredientItems;
+    }
+
+    public List<RequestedSeasoningItem> toRequestedSeasoningItems() {
+        List<RequestedSeasoningItem> requestedSeasoningItems = new ArrayList<>();
+        for (String seasoning : seasonings) {
+            RequestedSeasoningItem item = RequestedSeasoningItem.builder()
+                .name(seasoning)
+                .build();
+            requestedSeasoningItems.add(item);
+        }
+        return requestedSeasoningItems;
+    }
 
 }
