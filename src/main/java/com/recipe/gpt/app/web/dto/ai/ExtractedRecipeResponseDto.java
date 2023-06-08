c6cdbbc37f1e4d3d2f646f090ad74ef12827ccbd
min050410 <min050410@gmail.com>
2023-06-08 09:27:19 +0900

    refac: ExtractedRecipeResponseDto 파일 이동

---
+++ src/main/java/com/recipe/gpt/app/web/dto/ai/ExtractedRecipeResponseDto.java
@@ -0,0 +1,67 @@
+package com.recipe.gpt.app.web.dto.ai;
+
+import com.recipe.gpt.app.domain.recipe.ingredient.IngredientItem;
+import com.recipe.gpt.app.domain.recipe.procedure.ProcedureItem;
+import com.recipe.gpt.app.domain.recipe.seasoning.SeasoningItem;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class ExtractedRecipeResponseDto {
+
+    private List<ItemResponseDto> ingredients;
+
+    private List<ItemResponseDto> seasonings;
+
+    private List<String> recipe;
+
+    public List<IngredientItem> toIngredientItems() {
+        List<IngredientItem> ingredientItems = new ArrayList<>();
+        for (ItemResponseDto ingredient : ingredients) {
+            IngredientItem item = IngredientItem.builder()
+                .name(ingredient.getName())
+                .quantity(ingredient.getQuantity())
+                .build();
+            ingredientItems.add(item);
+        }
+        return ingredientItems;
+    }
+
+    public List<SeasoningItem> toSeasoningItems() {
+        List<SeasoningItem> seasoningItems = new ArrayList<>();
+        for (ItemResponseDto seasoning : seasonings) {
+            SeasoningItem item = SeasoningItem.builder()
+                .name(seasoning.getName())
+                .quantity(seasoning.getQuantity())
+                .build();
+            seasoningItems.add(item);
+        }
+        return seasoningItems;
+    }
+
+    public List<ProcedureItem> toProcedureItems() {
+        List<ProcedureItem> procedureItems = new ArrayList<>();
+        for (String recipe : recipe) {
+            ProcedureItem item = ProcedureItem.builder()
+                .description(recipe)
+                .build();
+            procedureItems.add(item);
+        }
+        return procedureItems;
+    }
+
+    public static ExtractedRecipeResponseDto of(AiServerRecipeResponseDto aiServerRecipeResponse) {
+        return new ExtractedRecipeResponseDto(
+            ItemResponseDto.listOf(aiServerRecipeResponse.getIngredients()),
+            ItemResponseDto.listOf(aiServerRecipeResponse.getSeasonings()),
+            aiServerRecipeResponse.getRecipe()
+        );
+    }
+
+}
