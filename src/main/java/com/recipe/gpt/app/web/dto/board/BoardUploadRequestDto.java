6979b771ce45c24a7d1b6302d13fe6b4c2ba449f
min050410 <min050410@gmail.com>
2023-06-12 14:07:33 +0900

    [#15] feat(Board): 게시글 업로드 dto 정의

---
+++ src/main/java/com/recipe/gpt/app/web/dto/board/BoardUploadRequestDto.java
@@ -0,0 +1,40 @@
+package com.recipe.gpt.app.web.dto.board;
+
+import com.recipe.gpt.app.domain.board.Board;
+import com.recipe.gpt.app.domain.board.Difficulty;
+import com.recipe.gpt.app.domain.member.Member;
+import com.recipe.gpt.app.web.dto.recipe.RecipeRequestDto;
+import com.recipe.gpt.common.validator.ValidEnum;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class BoardUploadRequestDto {
+
+    @ValidEnum(enumClass = Difficulty.class)
+    private Difficulty difficulty;
+
+    private long serving;
+
+    private long time;
+
+    @NotNull
+    private RecipeRequestDto recipe;
+
+    // TODO::이미지 URL
+    public Board toBoard(Member member) {
+        return Board.builder()
+            .member(member)
+            .serving(serving)
+            .time(time)
+            .difficulty(difficulty)
+            .imageUrl("example")
+            .build();
+    }
+
+}
---
+++ src/main/java/com/recipe/gpt/app/web/dto/recipe/RecipeRequestDto.java
@@ -0,0 +1,85 @@
+package com.recipe.gpt.app.web.dto.recipe;
+
+import com.recipe.gpt.app.domain.recipe.Recipe;
+import com.recipe.gpt.app.domain.recipe.ingredient.IngredientItem;
+import com.recipe.gpt.app.domain.recipe.procedure.ProcedureItem;
+import com.recipe.gpt.app.domain.recipe.seasoning.SeasoningItem;
+import com.recipe.gpt.app.web.dto.recipe.ingredient.IngredientRequestDto;
+import com.recipe.gpt.app.web.dto.recipe.seasoning.SeasoningRequestDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class RecipeRequestDto {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    @NotEmpty
+    @Size(max = 20)
+    private List<@NotNull IngredientRequestDto> ingredients;
+
+    @NotEmpty
+    @Size(max = 20)
+    private List<@NotNull SeasoningRequestDto> seasonings;
+
+    @NotEmpty
+    @Size(min = 1, max = 20)
+    private List<@NotBlank String> recipe;
+
+    public Recipe toRecipe() {
+        return Recipe.builder()
+            .name(name)
+            .description(description)
+            .build();
+    }
+
+    public List<IngredientItem> toIngredientItems() {
+        List<IngredientItem> ingredientItems = new ArrayList<>();
+        for (IngredientRequestDto ingredient : ingredients) {
+            IngredientItem item = IngredientItem.builder()
+                .name(ingredient.getName())
+                .quantity(ingredient.getQuantity())
+                .build();
+            ingredientItems.add(item);
+        }
+        return ingredientItems;
+    }
+
+    public List<SeasoningItem> toSeasoningItems() {
+        List<SeasoningItem> seasoningItems = new ArrayList<>();
+        for (SeasoningRequestDto seasoning : seasonings) {
+            SeasoningItem item = SeasoningItem.builder()
+                .name(seasoning.getName())
+                .quantity(seasoning.getQuantity())
+                .build();
+            seasoningItems.add(item);
+        }
+        return seasoningItems;
+    }
+
+    public List<ProcedureItem> toProcedureItems() {
+        List<ProcedureItem> procedureItems = new ArrayList<>();
+        for (String recipe : recipe) {
+            ProcedureItem item = ProcedureItem.builder()
+                .description(recipe)
+                .build();
+            procedureItems.add(item);
+        }
+        return procedureItems;
+    }
+
+}
