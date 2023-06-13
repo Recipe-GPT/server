e15a90e4eb08d482d5427046f0936cf0832a7ebf
min050410 <min050410@gmail.com>
2023-06-13 20:32:07 +0900

    fix: OpenAPIDefinition 추가

--- src/main/java/com/recipe/gpt/RecipeGptApplication.java
+++ src/main/java/com/recipe/gpt/RecipeGptApplication.java
@@ -1,11 +1,14 @@
 package com.recipe.gpt;
 
 import com.recipe.gpt.common.config.properties.GoogleProperties;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @EnableJpaAuditing
 @EnableConfigurationProperties(GoogleProperties.class)
 @SpringBootApplication
