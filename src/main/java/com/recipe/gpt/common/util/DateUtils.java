849dee2a33f2b56a0758d1017f92b683d6b5f33f
min050410 <min050410@gmail.com>
2023-06-13 15:15:20 +0900

    [#15] feat(Board): DateUtils format 함수

--- src/main/java/com/recipe/gpt/common/util/DateUtils.java
+++ src/main/java/com/recipe/gpt/common/util/DateUtils.java
@@ -1,5 +1,7 @@
 package com.recipe.gpt.common.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -13,6 +15,14 @@ public final class DateUtils {
 
     public static Date addTime(Date date, Long second) {
         return new Date(date.getTime() + second);
+    }
+
+    public static String formatToHyphenSeparatedDate(LocalDateTime createdDate) {
+        return createdDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+    public static String formatToDotSeparatedDate(LocalDateTime createdDate) {
+        return createdDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
     }
 
 }
