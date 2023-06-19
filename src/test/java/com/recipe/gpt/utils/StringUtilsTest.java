5ce522714ffcd4d88ff5a603da2e1e61a2df1c1b
min050410 <min050410@gmail.com>
2023-06-19 09:59:49 +0900

    refac: StringUtilsTest

--- src/test/java/com/recipe/gpt/utils/StringUtilsTest.java
+++ src/test/java/com/recipe/gpt/utils/StringUtilsTest.java
@@ -7,18 +7,24 @@ import org.junit.jupiter.api.Test;
 public class StringUtilsTest {
 
     @Test
-    public void testRemoveNumberedListPrefix() {
-
-        // 숫자 접두사가 있는 경우
+    public void removeNumberedListPrefixTest () {
+        // given
         String input1 = "1. 참치를 섞습니다";
         String expected1 = "참치를 섞습니다";
         String result1 = StringUtils.removeNumberedListPrefix(input1);
-        Assertions.assertEquals(expected1, result1);
 
-        // 숫자 접두사가 없는 경우
-        String input2 = "참치를 섞습니다";
+        String input2 = "1394. 참치를 섞습니다";
         String expected2 = "참치를 섞습니다";
         String result2 = StringUtils.removeNumberedListPrefix(input2);
+
+        String input3 = "참치를 섞습니다";
+        String expected3 = "참치를 섞습니다";
+        String result3 = StringUtils.removeNumberedListPrefix(input3);
+
+        // when & then
+        Assertions.assertEquals(expected1, result1);
         Assertions.assertEquals(expected2, result2);
+        Assertions.assertEquals(expected3, result3);
     }
+
 }
